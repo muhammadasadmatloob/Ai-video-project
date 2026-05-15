921ec131ac79f9e167eb40122c7e98b4914bc8ad
muhammadasadmatloob <muhammadasadmatloob214@gmail.com>
2026-05-15 10:54:40 +0500

    fix render ffmpeg

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,16 @@
 # Exit on error
 set -o errexit
 
+# 1. Install your Python packages
 pip install -r requirements.txt
 
-# Install FFmpeg for rendering
-apt-get update && apt-get install -y ffmpeg
+# 2. Download the full, uncut FFmpeg binary (This bypasses the apt-get error!)
+echo "Downloading full FFmpeg..."
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar -xf ffmpeg-release-amd64-static.tar.xz
+cp ffmpeg-*-amd64-static/ffmpeg ./ffmpeg
+chmod +x ./ffmpeg
+
+# 3. Download a free font (Roboto) so subtitles work on the server
+echo "Downloading Font..."
+wget -O font.ttf https://github.com/googlefonts/roboto/raw/main/src/hinted/Roboto-Black.ttf
